a6bdf5559e411b669e4b0940e941138ccd523f31
weshuiz13 <waarde10@zeelandnet.nl>
2021-07-18 16:10:20 +0200

    created layout file

---
+++ src/routes/admin/_layout.svelte
@@ -0,0 +1,11 @@
+<script context="module">
+    import { hasAdmin } from '../../util'
+	export async function preload(page, session) {
+		const { csrfToken, loggedIn, user } = session// get csrf token from session (set by sapper middleware see server.js)
+        if(loggedIn && hasAdmin(user)) {
+            return {}
+        }else {
+            return this.redirect(302, '/')// user not authorized redirect to 'home'
+        }
+	}
+</script>

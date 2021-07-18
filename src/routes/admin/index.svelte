9ee6975af9a20ad094dcfc595bb479dc4ac93044
weshuiz13 <waarde10@zeelandnet.nl>
2021-07-18 16:04:07 +0200

    added admin layout
    added layout for admin route

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -2,11 +2,7 @@
     import { hasAdmin } from '../../util'
 	export function preload(page, session) {
     const { csrfToken, loggedIn, user } = session// get csrf token from session (set by sapper middleware in server.js)
-        if( loggedIn && hasAdmin(user) ){
-            return { csrfToken, loggedIn, user }// authorized
-        }else {
-            return this.redirect(302, '/')// user not authorized redirect to 'home'
-        }
+        return { csrfToken, loggedIn, user }// authorized
 	}
 </script>
 

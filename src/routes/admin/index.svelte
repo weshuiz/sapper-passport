862ce016dd0ef4537ef9049c404c1ee2be637371
weshuiz13 <waarde10@zeelandnet.nl>
2021-07-18 16:08:33 +0200

    removed hasAdmin, moved admin verification layout
    exedently made the wrong comment for the wrong file, oops...

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -1,5 +1,4 @@
 <script context="module">
-    import { hasAdmin } from '../../util'
 	export function preload(page, session) {
     const { csrfToken, loggedIn, user } = session// get csrf token from session (set by sapper middleware in server.js)
         return { csrfToken, loggedIn, user }// authorized
